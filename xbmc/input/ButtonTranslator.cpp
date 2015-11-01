44d331de724158ada6e256870206c91375e5334e
aracnoz <aracnoz@yahoo.it>
2015-11-02 00:12:47 +0100

    [DSPLAYER] input

--- xbmc/input/ButtonTranslator.cpp
+++ xbmc/input/ButtonTranslator.cpp
@@ -382,6 +382,14 @@ static const ActionMapping windows[] =
     { "fullscreenradio"          , WINDOW_FULLSCREEN_RADIO },          // virtual window for fullscreen radio, uses WINDOW_VISUALISATION as fallback
     { "visualisation"            , WINDOW_VISUALISATION },
     { "slideshow"                , WINDOW_SLIDESHOW },
+#ifdef HAS_DS_PLAYER
+    {"dsrulesettings"           , WINDOW_DIALOG_DSRULES},
+    {"dsfiltersettings"         , WINDOW_DIALOG_DSFILTERS},
+    {"dsplayercoresettings"     , WINDOW_DIALOG_DSPLAYERCORE},
+    {"dsplayerlavvideo"         , WINDOW_DIALOG_LAVVIDEO},
+    {"dsplayerlavaudio"         , WINDOW_DIALOG_LAVAUDIO},
+    {"dsplayerlavsplitter"      , WINDOW_DIALOG_LAVSPLITTER},
+#endif
     { "weather"                  , WINDOW_WEATHER },
     { "screensaver"              , WINDOW_SCREENSAVER },
     { "videoosd"                 , WINDOW_DIALOG_VIDEO_OSD },
